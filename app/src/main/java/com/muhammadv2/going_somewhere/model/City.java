972443d7d79a43b53db74ae19bb128d046abe4d9
Muhammad2ali <muhammad2ali.ismail@gmail.com>
2018-01-31 16:54:30 +0200

    view 1 for adding new trip

--- app/src/main/java/com/muhammadv2/going_somewhere/model/City.java
+++ app/src/main/java/com/muhammadv2/going_somewhere/model/City.java
@@ -3,24 +3,17 @@ package com.muhammadv2.going_somewhere.model;
 import org.parceler.Parcel;
 import org.parceler.ParcelConstructor;
 
-import java.util.List;
-
 @Parcel
 public final class City {
 
     String cityName;
     int tripId;
-    List<Place> places;
+
 
     @ParcelConstructor
-    public City(String cityName, int tripId, List<Place> places) {
+    public City(String cityName, int tripId) {
         this.cityName = cityName;
         this.tripId = tripId;
-        this.places = places;
-    }
-
-    public List<Place> getPlaces() {
-        return places;
     }
 
     public String getCityName() {
--- app/src/main/java/com/muhammadv2/going_somewhere/trips/TripsFragment.java
+++ app/src/main/java/com/muhammadv2/going_somewhere/trips/TripsFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,9 +37,32 @@ public class TripsFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getActivity(),"Hello from Fragment",Toast.LENGTH_LONG).show();
+                Toast.makeText(getActivity(), "Hello from Fragment", Toast.LENGTH_LONG).show();
+                showDialog();
 
             }
         });
+    }
+
+    public void showDialog() {
+
+        boolean mIsLargeLayout = getResources().getBoolean(R.bool.large_layout);
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        TripAddDialog newFragment = new TripAddDialog();
+
+        if (mIsLargeLayout) {
+            // The device is using a large layout, so show the fragment as a dialog
+            newFragment.show(fragmentManager, "dialog");
+        } else {
+            // The device is smaller, so show the fragment fullscreen
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            // For a little polish, specify a transition animation
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+            // To make it fullscreen, use the 'content' root view as the container
+            // for the fragment, which is always the root view for the activity
+            transaction.add(R.id.root_view, newFragment)
+                    .addToBackStack(null).commit();
+        }
     }
 }
