ebfd98c7ca7bdab0a46017d7bbbc04d3f99adac2
Muhammad2ali <muhammad2ali.ismail@gmail.com>
2018-01-31 21:40:33 +0200

    add date picker for new trip fragment

--- app/src/main/java/com/muhammadv2/going_somewhere/ui/addTrip/AddTripFragment.java
+++ app/src/main/java/com/muhammadv2/going_somewhere/ui/addTrip/AddTripFragment.java
@@ -1,5 +1,7 @@
 package com.muhammadv2.going_somewhere.ui.addTrip;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,20 +14,29 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.muhammadv2.going_somewhere.R;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AddTripFragment extends Fragment {
 
-    @BindView(R.id.et_city_name)
-    EditText cityEntry;
-
+    // Associated views to this fragment
     @BindView(R.id.add_trip_container)
     ConstraintLayout container;
+    @BindView(R.id.et_city_name)
+    EditText cityEntry;
+    @BindView(R.id.et_trip_title)
+    EditText tripTitle;
+    @BindView(R.id.et_date_from)
+    EditText dateFrom;
+    @BindView(R.id.et_date_to)
+    EditText dateTo;
 
     /**
      * The system calls this to get the DialogFragment's layout, regardless
@@ -37,6 +48,7 @@ public class AddTripFragment extends Fragment {
         // Inflate the layout to use as dialog or embedded fragment
         View view = inflater.inflate(R.layout.fragment_add_trip, container, false);
 
+        // Bind ButterKnife library to this fragment
         ButterKnife.bind(this, view);
 
         return view;
@@ -46,42 +58,78 @@ public class AddTripFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        cityEntry.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        cityEntry.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View view, boolean b) {
-                String cityName = String.valueOf(cityEntry.getText());
-                if (cityName != null) {
-                    LayoutInflater inflater = (LayoutInflater)
-                            getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    final View rowView = inflater.inflate(R.layout.dynamic_add_city_field, null);
-                    // Add the new row before the add field button.
-                    container.addView(rowView, container.getChildCount() - 1);
-                }
-
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater)
+                        getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View rowView = inflater.inflate(R.layout.dynamic_add_city_field,
+                        container, false);
+                // Add the new row before the add field button.
+                container.addView(rowView, container.getChildCount() - 1);
             }
         });
 
+        dateFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                android.support.v4.app.DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+            }
+        });
+
+        dateTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                android.support.v4.app.DialogFragment newFragment = new DatePickerFragment();
+                newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+            }
+        });
+
+
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        getActivity().getMenuInflater().inflate(R.menu.menu_dialog, menu);
+    public static class DatePickerFragment extends android.support.v4.app.DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
 
-    }
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
         }
 
-        return super.onOptionsItemSelected(item);
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            getActivity().getMenuInflater().inflate(R.menu.menu_dialog, menu);
+
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
+
--- app/src/main/java/com/muhammadv2/going_somewhere/ui/addTrip/DataPickerFragment.java
+++
@@ -1,4 +0,0 @@
-package com.muhammadv2.going_somewhere.ui.addTrip;
-
-public class DataPickerFragment {
-}
